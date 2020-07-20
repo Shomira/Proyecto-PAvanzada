f45a4d8719651a784aa87276c820b95473963a38
Shomira <shomirarosales01@gmail.com>
2020-07-19 21:43:32 -0500

    Visualizaciones y Outliers

--- AvanceEDA_4.scala
+++ AvanceEDA_4.scala
@@ -118,3 +118,114 @@ display(data3Etnias.filter($"nivel_de_instruccion" === "09 - Superior Universita
 println(f"${(dataInd.filter($"nivel_de_instruccion" === "09 - Superior Universitario" ).where($"ingreso_laboral" < "400").count / dataInd.filter($"nivel_de_instruccion" === "09 - Superior Universitario").count.toDouble) * 100}%.2f%% Indigenas")
 println(f"${(dataMon.filter($"nivel_de_instruccion" === "09 - Superior Universitario" ).where($"ingreso_laboral" < "400").count / dataMon.filter($"nivel_de_instruccion" === "09 - Superior Universitario" ).count.toDouble) * 100}%.2f%% Montubio")
 println(f"${(dataMes.filter($"nivel_de_instruccion" === "09 - Superior Universitario" ).where($"ingreso_laboral" < "400").count / dataMes.filter($"nivel_de_instruccion" === "09 - Superior Universitario" ).count.toDouble) * 100}%.2f%% Mestizo")
+// COMMAND ----------
+
+// DBTITLE 1,Distribución de las Ramas de Actividad según cada Etnia
+display(data3Etnias.groupBy($"etnia").pivot($"rama_actividad").count.orderBy("etnia"))
+
+// COMMAND ----------
+
+// DBTITLE 1,Distribución de personas en cada rama de actividad de aquellos ubicados en el SECTOR INFORMAL según cada etnia
+display(data3Etnias.filter($"sectorizacion" === "2 - Sector Informal").groupBy($"rama_actividad").pivot($"etnia").count.orderBy("rama_actividad"))
+
+
+
+// COMMAND ----------
+
+// DBTITLE 1,Distribución de personas en cada rama de actividad que tengan un nivel de instrucción primaria según cada etnia
+display(data3Etnias.filter($"nivel_de_instruccion" === "04 - Primaria").groupBy($"rama_actividad").pivot($"etnia").count.orderBy("rama_actividad"))
+
+
+
+// COMMAND ----------
+
+// DBTITLE 1,Distribución de personas en cada rama de actividad que tengan un nivel de instrucción secundaria según cada etnia
+display(data3Etnias.filter($"nivel_de_instruccion" === "06 - Secundaria").groupBy($"rama_actividad").pivot($"etnia").count.orderBy("rama_actividad"))
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### OUTLIERS SUELDO
+
+// COMMAND ----------
+
+println("Data Indigenas")
+dataInd.select("ingreso_laboral").summary().show()
+
+val dfSueldoInd = dataInd.select("ingreso_laboral").where($"ingreso_laboral".isNotNull)
+val avgS = dfSueldoInd.select(mean("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val stdDesvS = dfSueldoInd.select(stddev("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val inferiorS = avgS - 3 * stdDesvS
+val superiorS = avgS + 3 * stdDesvS
+println("Data Montubios")
+
+dataMon.select("ingreso_laboral").summary().show()
+
+val dfSueldoMon = dataMon.select("ingreso_laboral").where($"ingreso_laboral".isNotNull)
+val avgSMon = dfSueldoMon.select(mean("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val stdDesvSMon = dfSueldoMon.select(stddev("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val inferiorSMon = avgSMon - 3 * stdDesvSMon
+val superiorSMon = avgSMon + 3 * stdDesvSMon
+println("Data Mestizos")
+dataMes.select("ingreso_laboral").summary().show()
+
+val dfSueldoMes = dataMes.select("ingreso_laboral").where($"ingreso_laboral".isNotNull)
+val avgSMes = dfSueldoMes.select(mean("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val stdDesvSMes = dfSueldoMes.select(stddev("ingreso_laboral")).first()(0).asInstanceOf[Double]
+val inferiorSMes = avgSMes - 3 * stdDesvSMes
+val superiorSMes = avgSMes + 3 * stdDesvSMes
+
+// COMMAND ----------
+
+
+val dataSinOutliersStdDesvSueldoInd  = dfSueldoInd.where($"ingreso_laboral" > inferiorS && $"ingreso_laboral" < superiorS)
+dataSinOutliersStdDesvSueldoInd.describe().show
+//
+
+val dataSinOutliersStdDesvSueldoMon  = dfSueldoMon.where($"ingreso_laboral" > inferiorSMon && $"ingreso_laboral" < superiorSMon)
+dataSinOutliersStdDesvSueldoMon.describe().show
+//
+
+val dataSinOutliersStdDesvSueldoMes  = dfSueldoMes.where($"ingreso_laboral" > inferiorSMes && $"ingreso_laboral" < superiorSMes)
+dataSinOutliersStdDesvSueldoMes.describe().show
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### OUTLIERS EDAD
+
+// COMMAND ----------
+
+println("Etnia Indigena")
+dataInd.select("edad").summary().show()
+val dfEdadInd = dataInd.select("edad").where($"edad".isNotNull)
+val avgE = dfEdadInd.select(mean("edad")).first()(0).asInstanceOf[Double]
+val stdDesvE = dfEdadInd.select(stddev("edad")).first()(0).asInstanceOf[Double]
+val inferiorE = avgE - 3 * stdDesvE
+val superiorE = avgE + 3 * stdDesvE
+println("Etnia Montubio")
+dataMon.select("edad").summary().show()
+val dfEdadMon = dataMon.select("edad").where($"edad".isNotNull)
+val avgEMon = dfEdadMon.select(mean("edad")).first()(0).asInstanceOf[Double]
+val stdDesvEMon = dfEdadMon.select(stddev("edad")).first()(0).asInstanceOf[Double]
+val inferiorEMon = avgEMon  - 3 * stdDesvEMon
+val superiorEMon = avgEMon + 3 * stdDesvEMon
+println("Etnia Mestizo")
+dataMes.select("edad").summary().show()
+val dfEdadMes = dataMes.select("edad").where($"edad".isNotNull)
+val avgEMes = dfEdadMes.select(mean("edad")).first()(0).asInstanceOf[Double]
+val stdDesvEMes = dfEdadMes.select(stddev("edad")).first()(0).asInstanceOf[Double]
+val inferiorEMes = avgEMes - 3 * stdDesvEMes
+val superiorEMes = avgEMes + 3 * stdDesvEMes
+
+// COMMAND ----------
+
+val dataSinOutliersStdDesvEdadInd = dfEdadInd.where($"edad" > inferiorE && $"edad" < superiorE)
+dataSinOutliersStdDesvEdadInd.describe().show
+//
+val dataSinOutliersStdDesvEdadMon = dfEdadMon.where($"edad" > inferiorEMon && $"edad" < superiorEMon)
+dataSinOutliersStdDesvEdadMon.describe().show
+//
+val dataSinOutliersStdDesvEdadMes = dfEdadMes.where($"edad" > inferiorEMes && $"edad" < superiorEMes)
+dataSinOutliersStdDesvEdadMes.describe().show
